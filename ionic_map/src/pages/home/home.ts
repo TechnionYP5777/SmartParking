af5be76c5b0d9fbc7db2802fdbcd1b90344be193
shaysegal <siglons@gmail.com>
2017-04-05 19:26:33 +0300

    add ionic map files @shaysegal #24

---
+++ ionic_map/src/pages/home/home.ts
@@ -0,0 +1,34 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+declare var google;
+ 
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+ 
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+  geolocation: Geolocation
+  constructor(public navCtrl: NavController) {}
+  
+  ionViewDidLoad(){
+    this.loadMap();
+  }
+ loadMap(){
+	this.geolocation = new Geolocation();
+	this.geolocation.getCurrentPosition().then((position) => {
+        let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        let mapOptions = {
+            center: latLng,
+            zoom: 15,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+         }
+        this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+       }, (err) => {
+           console.log(err);
+       });
+  } 
+}
