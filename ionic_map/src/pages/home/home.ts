1803c76633832410e2b31daef6f926264498ce71
sefialbo <sefialbo@gmail.com>
2017-04-06 11:00:46 +0300

    add route on map @sefialbo #24

--- ionic_map/src/pages/home/home.ts
+++ ionic_map/src/pages/home/home.ts
@@ -12,11 +12,45 @@ export class HomePage {
   @ViewChild('map') mapElement: ElementRef;
   map: any;
   geolocation: Geolocation
+  source: any;
+  destination: any;
+  directionsService: any;
+  directionsDisplay: any;
+  i: number;
   constructor(public navCtrl: NavController) {}
   
   ionViewDidLoad(){
-    this.loadMap();
+    this.loadMap2();
   }
+  loadMap2() {
+     this.directionsService = new google.maps.DirectionsService;
+     this.directionsDisplay = new google.maps.DirectionsRenderer;
+     var map = new google.maps.Map(this.mapElement.nativeElement, {
+        zoom: 15,
+        center: {lat: 41.85, lng: -87.65}
+     });
+     this.geolocation = new Geolocation();
+     this.geolocation.getCurrentPosition().then((position) => {
+        let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        map.setCenter(latLng);
+     });
+     this.i = 0;
+     map.addListener('click',(e) => this.placeMarkerAndPanTo(e.latLng, map));
+     this.directionsDisplay.setMap(map);
+  }
+  calculateAndDisplayRoute(directionsService, directionsDisplay) {
+        directionsService.route({
+          origin: this.source.position,
+          destination: this.destination.position,
+          travelMode: 'DRIVING'
+        }, function(response, status) {
+          if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
+  }  
   loadMap(){
 	this.geolocation = new Geolocation();
 	this.geolocation.getCurrentPosition().then((position) => {
@@ -37,6 +71,15 @@ export class HomePage {
      position: latLng,
      map: map
     });
+    if ( this.i%2 == 0) {
+        this.source = marker;
+    } else {
+        this.destination = marker;
+    }
+    if ( this.i > 0 ) {
+       this.calculateAndDisplayRoute(this.directionsService, this.directionsDisplay);
+    }
+    this.i++;
     map.panTo(latLng);
   } 
 }
