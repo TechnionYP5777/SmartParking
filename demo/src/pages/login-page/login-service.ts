aec96f89b0532cb8a2882f8eab1c6d3e1ea1bc33
= <dcohen484@gmail.com>
2017-05-08 00:46:26 +0300

    forgot to upload.. #34 #39

---
+++ demo/src/pages/login-page/login-service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
+
+@Injectable()
+export class LoginService {
+  constructor(public http: Http) {
+    console.log('Hello LoginService Provider');
+  }
+  callHttp() {
+    return this.http.get('http://localhost:8080/shahar').map(res => res.json())
+      .catch(this.handleError);
+  }
+  
+  handleError(error) {
+    console.error(error);
+    return Observable.throw('Server error');
+  }
+
+  tempLogin(user) {
+    var value = "name=" + user;
+
+    var headers = new Headers();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+
+    return new Promise(resolve => {
+      this.http.post('http://localhost:8080/User', value, { headers: headers }).subscribe(data => {
+        if (data.status == 200) {
+          resolve(true);
+        }
+        else
+          resolve(false);
+      });
+    });
+  }
+
+}
