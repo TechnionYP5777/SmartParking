5da73705957b6f8438aa5fe2f65dc65fa0202b5d
= <dcohen484@gmail.com>
2017-05-17 13:00:51 +0300

    Add service to get parking slots #42

--- src/rest_test/LocationController.java
+++ src/rest_test/LocationController.java
@@ -5,7 +5,11 @@ import org.springframework.web.bind.annotation.*;
 import main.java.data.management.DBManager;
 import main.java.data.members.Destination;
 import main.java.data.members.MapLocation;
+import main.java.data.members.ParkingSlot;
+import main.java.data.members.ParkingSlotStatus;
+import main.java.data.members.StickersColor;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,6 +21,8 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class LocationController {
+	Map<String, MapLocation> hmap;
+	ArrayList<ServerParkingSlot> parkingList;
 
 	public LocationController() {
 		DBManager.initialize();
@@ -25,9 +31,8 @@ public class LocationController {
 	@RequestMapping(value = "/Locations", produces = "application/json")
 	@ResponseBody
 	public String getLocations() {
-
 		final ParseQuery<ParseObject> query = ParseQuery.getQuery("Destination");
-		Map<String, MapLocation> hmap = new HashMap<String, MapLocation>();
+		hmap = new HashMap<String, MapLocation>();
 		try {
 			final List<ParseObject> result = query.find();
 			if (result == null)
@@ -39,8 +44,23 @@ public class LocationController {
 		} catch (final Exception e) {
 			System.out.println("exception...");
 		}
-
-		//System.out.println(hmap);
 		return new JSONObject(hmap) + "";
+	}
+
+	@RequestMapping(value = "/ParkingSlots", produces = "application/json")
+	@ResponseBody
+	public ArrayList<ServerParkingSlot> getParkingSlots() {
+		final ParseQuery<ParseObject> query = ParseQuery.getQuery("ParkingSlot");
+		parkingList = new ArrayList<ServerParkingSlot>();
+		try {
+			final List<ParseObject> result = query.find();
+			if (result == null)
+				System.out.println("empty");
+			for (final ParseObject park : result)
+				parkingList.add(new ServerParkingSlot(new ParkingSlot(park)));
+		} catch (final Exception e) {
+			System.out.println("exception...");
+		}
+		return parkingList;
 	}
 }
