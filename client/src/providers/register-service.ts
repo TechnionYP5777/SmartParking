26d77eda86b9880c80ea2e2b06f0794a72f0fc2a
DavidCohen55 <dcohen484@gmail.com>
2017-05-17 14:35:33 +0300

    Started working on register service.Still work to be done. #43

---
+++ client/src/providers/register-service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/Rx';
+
+@Injectable()
+export class RegisterService {
+    constructor(public http: Http) {
+        console.log('Hello RegisterService Provider');
+    }
+   
+    userRegister() {
+        return this.http.get('http://localhost:8080/User/Register').map(res => res.json()).subscribe(data => {
+            console.log("get data: RegisterService");
+        });
+    }
+}
