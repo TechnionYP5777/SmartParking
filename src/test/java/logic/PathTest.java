f88eedd98233b0f665136e1a2191416a8aeeed32
= <dcohen484@gmail.com>
2017-05-25 18:46:23 +0300

    added a tests #46

--- src/test/java/logic/PathTest.java
+++ src/test/java/logic/PathTest.java
@@ -11,20 +11,20 @@ import org.parse4j.ParseException;
 
 import main.java.Exceptions.AlreadyExists;
 import main.java.Exceptions.NotExists;
-import main.java.data.members.Destination;
 import main.java.data.members.MapLocation;
 import main.java.data.members.RootPath;
 import main.java.util.LogPrinter;
 
 public class PathTest {
 
+	// check that the constructor works
 	@Test
 	public void test01() {
 		ArrayList<MapLocation> mp = new ArrayList<MapLocation>();
 		mp.add(new MapLocation(23, 23));
 		mp.add(new MapLocation(24, 24));
 		mp.add(new MapLocation(25, 25));
-		RootPath p = null ;
+		RootPath p = null;
 		try {
 			p = new RootPath("Ulman", "Sports Center", mp);
 			Assert.assertEquals("Ulman", p.getSource());
@@ -40,26 +40,27 @@ public class PathTest {
 			LogPrinter.createLogFile(e);
 			Assert.fail();
 		} catch (AlreadyExists e) {
-				Assert.assertEquals("Already exists", e.getMessage());
+			Assert.assertEquals("Already exists", e.getMessage());
 		}
-		
+
 		try {
 			p.deleteParseObject();
 		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			LogPrinter.createLogFile(e);
+			Assert.fail();
 		}
 
 	}
 
+	// check that the constructor gets rootPath by src and dest
 	@Test
 	public void test02() {
-		
+
 		ArrayList<MapLocation> mp = new ArrayList<MapLocation>();
 		mp.add(new MapLocation(23, 23));
 		mp.add(new MapLocation(24, 24));
 		mp.add(new MapLocation(25, 25));
-		RootPath p = null ;
+		RootPath p = null;
 		try {
 			p = new RootPath("Ulman", "Sports Center", mp);
 			RootPath r = new RootPath("Ulman", "Sports Center");
@@ -72,4 +73,40 @@ public class PathTest {
 			Assert.fail();
 		}
 	}
+
+	@Test
+	public void test03() {
+		ArrayList<MapLocation> mp = new ArrayList<MapLocation>();
+		RootPath p = null;
+		try {
+			p = new RootPath("Ulman", "Sports Center", mp);
+			p.setSource("Computer Science Faculty");
+			p.setDestination("Industrial Engineering Faculty");
+			mp.add(new MapLocation(23, 23));
+			p.setRoot(mp);
+			Assert.assertFalse(RootPath.PathExists("Ulman", "Sports Center"));
+			Assert.assertTrue(RootPath.PathExists("Computer Science Faculty", "Industrial Engineering Faculty"));
+			RootPath r = new RootPath("Computer Science Faculty", "Industrial Engineering Faculty");
+			Assert.assertEquals(r.getSource(), p.getSource());
+			Assert.assertEquals(r.getDestination(), p.getDestination());
+			Assert.assertEquals(r.getRoot().size(), p.getRoot().size());
+			p.deleteParseObject();
+		} catch (ParseException | AlreadyExists | NotExists e) {
+			LogPrinter.createLogFile(e);
+			Assert.fail();
+		}
+	}
+	
+	// check that the root isn't found
+	@Test
+	public void test04() {
+		try {
+			RootPath p = new RootPath("Computer Science Faculty", "Industrial Engineering Faculty");
+		} catch (ParseException | AlreadyExists e) {
+			LogPrinter.createLogFile(e);
+			Assert.fail();
+		} catch (NotExists e) {
+			Assert.assertEquals("Didn't find the path", e.getMessage());
+		}
+	}	
 }
