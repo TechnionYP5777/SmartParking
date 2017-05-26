b8ac1901e7636819f365ab2b990d0344177df884
= <dcohen484@gmail.com>
2017-05-27 01:54:22 +0300

    Change the class RootPath and tests #46

--- src/main/java/data/members/RootPath.java
+++ src/main/java/data/members/RootPath.java
@@ -1,10 +1,7 @@
 package main.java.data.members;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 import org.parse4j.ParseException;
 import org.parse4j.ParseGeoPoint;
@@ -13,7 +10,6 @@ import org.parse4j.ParseQuery;
 
 import main.java.Exceptions.*;
 import main.java.data.management.DBManager;
-import main.java.util.LogPrinter;
 
 /**
  * @Author DavidCohen55
@@ -22,10 +18,14 @@ public class RootPath extends dbMember {
 	private String source;
 	private String destination;
 	private ArrayList<MapLocation> root;
+	private double duration;
+	private String description;
 
 	private static final String SOURCE = "source";
 	private static final String DESTINATION = "destination";
 	private static final String ROOT = "Root";
+	private static final String DURATION = "Duration";
+	private static final String DESCRIPTION = "Description";
 	private static final String TABLE_NAME = "Path";
 
 	private static ParseObject getPathParse(final String src, final String dest) {
@@ -40,26 +40,33 @@ public class RootPath extends dbMember {
 			return null;
 		}
 	}
-	
+
 	public static boolean PathExists(final String src, final String dest) {
 		return getPathParse(src, dest) != null;
 	}
 
-	public RootPath(final String src, final String dest, final ArrayList<MapLocation> root)
+	public RootPath(final String src, final String dest, final ArrayList<MapLocation> root, int dur, String desc)
 			throws ParseException, AlreadyExists, NotExists {
 		DBManager.initialize();
-		setParseObject(TABLE_NAME);
 
 		if (!ParkingArea.areaExists(src) && !Destination.destinationExists(dest))
 			throw new NotExists("Didn't find src or dest");
 
-		if (PathExists(src, dest))
-			throw new AlreadyExists("Already exists");
+		ParseObject po = getPathParse(src, dest);
+		if (po == null)
+			setParseObject(TABLE_NAME);
+		else {
+			if (po.getDouble(DURATION) < dur)
+				throw new AlreadyExists("Better path exists");
+			this.parseObject = po;
+		}
 
 		this.setSource(src);
 		this.setDestination(dest);
 		this.setRoot(root);
-		setObjectId();
+		this.setDuration(dur);
+		this.setDescription(desc);
+		this.setObjectId();
 	}
 
 	public RootPath(final String src, final String dest) throws ParseException, AlreadyExists, NotExists {
@@ -83,6 +90,9 @@ public class RootPath extends dbMember {
 		this.setObjectId();
 		this.setSource(this.parseObject.getString(SOURCE));
 		this.setDestination(this.parseObject.getString(DESTINATION));
+		this.setDuration(this.parseObject.getDouble(DURATION));
+		this.setDescription(this.parseObject.getString(DESCRIPTION));
+		
 		this.root = new ArrayList<MapLocation>();
 		List<Object> points = this.parseObject.getList(ROOT);
 		for (Object o : points)
@@ -99,7 +109,7 @@ public class RootPath extends dbMember {
 		parseObject.save();
 	}
 
-	public void setSource(final String src) throws ParseException, NotExists{
+	public void setSource(final String src) throws ParseException, NotExists {
 		if (!ParkingArea.areaExists(src))
 			throw new NotExists("Didn't find source");
 		this.source = src;
@@ -116,6 +126,18 @@ public class RootPath extends dbMember {
 		parseObject.save();
 	}
 
+	public void setDuration(double dur) throws ParseException {
+		this.duration = dur;
+		parseObject.put(DURATION, dur);
+		parseObject.save();
+	}
+
+	public void setDescription(String desc) throws ParseException {
+		this.description = desc;
+		parseObject.put(DESCRIPTION, desc);
+		parseObject.save();
+	}
+
 	public String getSource() {
 		return this.source;
 	}
@@ -128,4 +150,11 @@ public class RootPath extends dbMember {
 		return root;
 	}
 
+	public double getDuration() {
+		return this.duration;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
 }
--- src/test/java/logic/PathTest.java
+++ src/test/java/logic/PathTest.java
@@ -1,11 +1,10 @@
 package test.java.logic;
 
-import static org.junit.Assert.*;
-
-import java.nio.file.Path;
 import java.util.ArrayList;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.parse4j.ParseException;
 
@@ -17,80 +16,98 @@ import main.java.util.LogPrinter;
 
 public class PathTest {
 
+	@Before
+	public void BeforePathTest() {
+		ArrayList<MapLocation> mp = new ArrayList<MapLocation>();
+		mp.add(new MapLocation(23, 23));
+		mp.add(new MapLocation(24, 24));
+		mp.add(new MapLocation(25, 25));
+		try {
+			new RootPath("Makak", "Sports Center", mp, 5, "Turn Left and then Straight");
+		} catch (ParseException | AlreadyExists | NotExists e) {
+			LogPrinter.createLogFile(e);
+			Assert.fail();
+		}
+	}
+
+	@After
+	public void AfterPathTest() {
+		try {
+			new RootPath("Makak", "Sports Center").deleteParseObject();
+		} catch (ParseException | AlreadyExists | NotExists e) {
+			LogPrinter.createLogFile(e);
+			Assert.fail();
+		}
+
+	}
+
 	// check that the constructor works
 	@Test
 	public void test01() {
 		ArrayList<MapLocation> mp = new ArrayList<MapLocation>();
 		mp.add(new MapLocation(23, 23));
 		mp.add(new MapLocation(24, 24));
-		mp.add(new MapLocation(25, 25));
-		RootPath p = null;
+
 		try {
-			p = new RootPath("Makak", "Sports Center", mp);
+			RootPath p = new RootPath("Makak", "Sports Center");
 			Assert.assertEquals("Makak", p.getSource());
 			Assert.assertEquals("Sports Center", p.getDestination());
 			Assert.assertEquals(3, p.getRoot().size());
+			Assert.assertTrue(p.getDuration() == 5);
+			Assert.assertEquals("Turn Left and then Straight", p.getDescription());
+
 		} catch (ParseException | AlreadyExists | NotExists e) {
 			LogPrinter.createLogFile(e);
 			Assert.fail();
 		}
+
 		try {
-			RootPath p2 = new RootPath("Makak", "Sports Center", mp);
+			RootPath p2 = new RootPath("Makak", "Sports Center", mp, 1, "Turn Right");
+			Assert.assertEquals("Makak", p2.getSource());
+			Assert.assertEquals("Sports Center", p2.getDestination());
+			Assert.assertTrue(p2.getDuration() == 1);
+			Assert.assertEquals(2, p2.getRoot().size());
+			Assert.assertEquals("Turn Right", p2.getDescription());
+		} catch (AlreadyExists | ParseException | NotExists e) {
+			LogPrinter.createLogFile(e);
+			Assert.fail();
+		}
+
+		try {
+			new RootPath("Makak", "Sports Center", mp, 9, "Turn Right");
 		} catch (ParseException | NotExists e) {
 			LogPrinter.createLogFile(e);
 			Assert.fail();
 		} catch (AlreadyExists e) {
-			Assert.assertEquals("Already exists", e.getMessage());
-		}
-
-		try {
-			p.deleteParseObject();
-		} catch (ParseException e) {
-			LogPrinter.createLogFile(e);
-			Assert.fail();
-		}
-
-	}
-
-	// check that the constructor gets rootPath by src and dest
-	@Test
-	public void test02() {
-
-		ArrayList<MapLocation> mp = new ArrayList<MapLocation>();
-		mp.add(new MapLocation(23, 23));
-		mp.add(new MapLocation(24, 24));
-		mp.add(new MapLocation(25, 25));
-		RootPath p = null;
-		try {
-			p = new RootPath("Makak", "Sports Center", mp);
-			RootPath r = new RootPath("Makak", "Sports Center");
-			Assert.assertEquals(r.getSource(), p.getSource());
-			Assert.assertEquals(r.getDestination(), p.getDestination());
-			Assert.assertEquals(r.getRoot().size(), p.getRoot().size());
-			p.deleteParseObject();
-		} catch (ParseException | AlreadyExists | NotExists e) {
-			LogPrinter.createLogFile(e);
-			Assert.fail();
+			Assert.assertEquals("Better path exists", e.getMessage());
 		}
 	}
 
 	// check that the setters work
 	@Test
-	public void test03() {
+	public void test02() {
 		ArrayList<MapLocation> mp = new ArrayList<MapLocation>();
+		mp.add(new MapLocation(23, 23));
 		RootPath p = null;
+
 		try {
-			p = new RootPath("Makak", "Sports Center", mp);
+			p = new RootPath("Makak", "Sports Center");
 			p.setSource("Pool");
 			p.setDestination("Industrial Engineering Faculty");
-			mp.add(new MapLocation(23, 23));
+			p.setDescription("Turn Right");
+			p.setDuration(3);
 			p.setRoot(mp);
+
 			Assert.assertFalse(RootPath.PathExists("Makak", "Sports Center"));
 			Assert.assertTrue(RootPath.PathExists("Pool", "Industrial Engineering Faculty"));
+
 			RootPath r = new RootPath("Pool", "Industrial Engineering Faculty");
 			Assert.assertEquals(r.getSource(), p.getSource());
 			Assert.assertEquals(r.getDestination(), p.getDestination());
 			Assert.assertEquals(r.getRoot().size(), p.getRoot().size());
+			Assert.assertTrue(r.getDuration() == p.getDuration());
+			Assert.assertEquals(r.getDescription(), p.getDescription());
+
 		} catch (ParseException | AlreadyExists | NotExists e) {
 			LogPrinter.createLogFile(e);
 			Assert.fail();
@@ -103,7 +120,6 @@ public class PathTest {
 			Assert.fail();
 		} catch (NotExists e) {
 			Assert.assertEquals("Didn't find source", e.getMessage());
-
 		}
 
 		try {
@@ -114,10 +130,12 @@ public class PathTest {
 		} catch (NotExists e) {
 			Assert.assertEquals("Didn't find destination", e.getMessage());
 		}
-
+		
 		try {
-			p.deleteParseObject();
-		} catch (ParseException e) {
+			p.setSource("Makak");
+			p.setDestination("Sports Center");
+
+		} catch (ParseException | NotExists e) {
 			LogPrinter.createLogFile(e);
 			Assert.fail();
 		}
@@ -125,9 +143,10 @@ public class PathTest {
 
 	// check that the root isn't found
 	@Test
-	public void test04() {
+	public void test03() {
 		try {
-			RootPath p = new RootPath("Pool", "Industrial Engineering Faculty");
+			new RootPath("Pool", "Industrial Engineering Faculty");
+			throw new AlreadyExists("Path already exists");
 		} catch (ParseException | AlreadyExists e) {
 			LogPrinter.createLogFile(e);
 			Assert.fail();
