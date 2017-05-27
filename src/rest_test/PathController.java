52ed08fa9739f4df6ef3b38b4a93efb4a97851d3
= <dcohen484@gmail.com>
2017-05-27 17:11:08 +0300

    Get the path from DB using src and dest #44

--- src/rest_test/PathController.java
+++ src/rest_test/PathController.java
@@ -38,7 +38,7 @@ public class PathController {
 	@CrossOrigin(origins = "http://localhost:8100")
 	@RequestMapping(value = "/GetPath", produces = "application/json")
 	@ResponseBody
-	public String getLocations(@RequestParam("area") String area, @RequestParam("dest") String dest) {
+	public String getPath(@RequestParam("area") String area, @RequestParam("dest") String dest) {
 
 		JSONObject obj = new JSONObject();
 		try {
@@ -49,6 +49,32 @@ public class PathController {
 			obj.put("Description", root.getDescription());
 			
 			
+			
+			JSONArray list = new JSONArray();
+			ArrayList<MapLocation> path = root.getRoot();
+			for (MapLocation mapLocation : path){
+				JSONObject loc = new JSONObject();
+				loc.put("lat", mapLocation.getLat());
+				loc.put("lon", mapLocation.getLon());
+				list.put(loc);
+			}
+
+			obj.put("Root", list);
+
+		} catch (ParseException | NotExists e) {
+			obj.put("error", e.getMessage());
+		}
+		return obj + "";
+	}
+	
+	@CrossOrigin(origins = "http://localhost:8100")
+	@RequestMapping(value = "/GetRoot", produces = "application/json")
+	@ResponseBody
+	public String getRoot(@RequestParam("area") String area, @RequestParam("dest") String dest) {
+
+		JSONObject obj = new JSONObject();
+		try {
+			RootPath root = new RootPath(area, dest);			
 			
 			JSONArray list = new JSONArray();
 			ArrayList<MapLocation> path = root.getRoot();
