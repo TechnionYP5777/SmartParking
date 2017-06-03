99171abbdb35d36fa82b7102e9e4dffc7a755030
Shahar-Y <shahar95@campus.technion.ac.il>
2017-06-03 16:08:35 +0300

    Last commit didn't work properly. Made some progress with register service, but not finished yet. #43

--- client/src/providers/register-service.ts
+++ client/src/providers/register-service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
 import 'rxjs/Rx';
+
 
 @Injectable()
 export class RegisterService {
@@ -9,7 +10,21 @@ export class RegisterService {
         console.log('Hello RegisterService Provider');
     }
    
-    userRegister() {
-        return this.http.get('http://localhost:8080/Register').map(res => res.json());
+    userRegister(userName, password, phoneNum, carNum, eMail, stickerColor) {
+      /*
+       * (@RequestParam("name") String name, @RequestParam("pass") String pass,
+      @RequestParam("phone") String phone, @RequestParam("car") String car, @RequestParam("email") String email,
+      @RequestParam("type") int type)
+       * 
+       */
+      var value = "name=" + userName + "&pass=" + password + "&phone=" +
+         phoneNum + "&car=" + password + "@email=" + eMail + "&type=2";
+        console.log("in userRegister, value = " + value);
+        var headers = new Headers();
+        headers.append('Content-Type', 'application/x-www-form-urlencoded');
+
+        return this.http.post('http://localhost:8080/User/Register', value, { headers: headers })
+            .map(res => res.json());
+      
     }
 }
