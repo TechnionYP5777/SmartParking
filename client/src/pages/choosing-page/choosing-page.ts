9aec7e15552c22f128c509f74e6cb03648b7c480
zahimizrahi <zahimizrahi1@gmail.com>
2017-06-08 20:46:01 +0300

    changed navigation bar & choosing-page gui design #57

--- client/src/pages/choosing-page/choosing-page.ts
+++ client/src/pages/choosing-page/choosing-page.ts
@@ -2,20 +2,20 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { LocationService } from '../../providers/location-service';
 declare var google;
- /**
- * Generated class for the ChoosingPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+/**
+* Generated class for the ChoosingPage page.
+*
+* See http://ionicframework.com/docs/components/#navigation for more info
+* on Ionic pages and navigation.
+*/
 @IonicPage()
 @Component({
   selector: 'page-choosing-page',
   templateUrl: 'choosing-page.html'
 })
 export class ChoosingPage {
-  sources:Array<{title: string, position:any}>;
-  dests:Array<{title: string, position:any}>;
+  sources: Array<{ title: string, position: any }>;
+  dests: Array<{ title: string, position: any }>;
   sourceLoc: any;
   destLoc: any;
   srcCallBack: any;
@@ -23,42 +23,42 @@ export class ChoosingPage {
   goCallBack: any;
   mapPage: any;
   googleObj: any;
-  constructor(public navCtrl: NavController, public navParams: NavParams, public locService: LocationService ) {
-        this.googleObj = navParams.get('googleObj');
-        this.sources=[];
-        this.dests=[];
-        locService.getLocations(this.sources,this.dests,this.googleObj);
-        this.mapPage = navParams.get('mapPage');
-        google.maps.event.clearListeners(this.mapPage.mapView, 'mousemove');
-   
+  constructor(public navCtrl: NavController, public navParams: NavParams, public locService: LocationService) {
+    this.googleObj = navParams.get('googleObj');
+    this.sources = [];
+    this.dests = [];
+    locService.getLocations(this.sources, this.dests, this.googleObj);
+    this.mapPage = navParams.get('mapPage');
+    google.maps.event.clearListeners(this.mapPage.mapView, 'mousemove');
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ChoosingPage');
   }
   goback() {
-     console.log("here");
-     this.mapPage.go();
-     this.navCtrl.pop();
+    console.log("here");
+    this.mapPage.go();
+    this.navCtrl.pop();
   }
-  rememberDest(dict:any){
-        this.mapPage.setDstPosition(dict["position"],dict["title"]);
+  rememberDest(dict: any) {
+    this.mapPage.setDstPosition(dict["position"], dict["title"]);
   }
-  rememberSrc(dict:any){
-        this.mapPage.setSrcPosition(dict["position"]);
+  rememberSrc(dict: any) {
+    this.mapPage.setSrcPosition(dict["position"]);
   }
-  changeToRecord(value:any){
-	this.mapPage.wantRecordRoute=value.checked;
-  	console.log(value);
+  changeToRecord(value: any) {
+    this.mapPage.wantRecordRoute = value.checked;
+    console.log(value);
   }
-  changeToSimulation(value:any){
-    if(value.checked){
-         google.maps.event.addListener(this.mapPage.mapView, 'mousemove', function (event) {
-                  console.log(event.latLng);
-         }); 
+  changeToSimulation(value: any) {
+    if (value.checked) {
+      google.maps.event.addListener(this.mapPage.mapView, 'mousemove', function(event) {
+        console.log(event.latLng);
+      });
     }
-    else{
-        google.maps.event.clearListeners(this.mapPage.mapView, 'mousemove');
+    else {
+      google.maps.event.clearListeners(this.mapPage.mapView, 'mousemove');
     }
   }
 }
