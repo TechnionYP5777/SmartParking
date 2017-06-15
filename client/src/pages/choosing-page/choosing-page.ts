e8da3796a08e2d8badcc2c5561cbad4773bfbd06
zahimizrahi <zahimizrahi1@gmail.com>
2017-06-15 20:25:45 +0300

    adding searchbar with autocomplete #61

--- client/src/pages/choosing-page/choosing-page.ts
+++ client/src/pages/choosing-page/choosing-page.ts
@@ -23,6 +23,8 @@ export class ChoosingPage {
   goCallBack: any;
   mapPage: any;
   googleObj: any;
+  showSrcList = false;  
+  showDestList = false; 
   constructor(public navCtrl: NavController, public navParams: NavParams, public locService: LocationService) {
     this.googleObj = navParams.get('googleObj');
     this.sources = [];
@@ -41,12 +43,43 @@ export class ChoosingPage {
     this.mapPage.go();
     this.navCtrl.pop();
   }
-  rememberDest(dict: any) {
-    this.mapPage.setDstPosition(dict["position"], dict["title"]);
+  
+   getItemsDest ( ev: any) {
+        let val = ev.target.value; 
+    if (val && val.trim!='') {
+      
+      this.dests=this.dests.filter((p) => {
+        return (p.title.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      });
+      this.showDestList=true; 
+    } else {
+      this.showDestList = false; 
   }
-  rememberSrc(dict: any) {
+}
+  
+    
+  rememberDest(dict: any) {
+     this.mapPage.setDstPosition(dict["position"], dict["title"]);
+  }
+  
+  getItemsSrc ( ev: any) {
+        let val = ev.target.value; 
+    if (val && val.trim!='') {
+      
+      this.sources=this.sources.filter((p) => {
+        return (p.title.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      });
+      this.showSrcList = true; 
+    } else {
+      this.showSrcList = false; 
+  }
+}
+
+  rememberSrc (dict: any ) {
     this.mapPage.setSrcPosition(dict["position"]);
   }
+    
+    
   changeToRecord(value: any) {
     this.mapPage.wantRecordRoute = value.checked;
     console.log(value);
