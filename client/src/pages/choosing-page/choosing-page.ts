5603be5832cb6511fe9d234b90c7bb1d7dbca3eb
zahimizrahi <zahimizrahi1@gmail.com>
2017-06-24 00:04:39 +0300

    finishing issues #61 & #62. closing them

--- client/src/pages/choosing-page/choosing-page.ts
+++ client/src/pages/choosing-page/choosing-page.ts
@@ -14,7 +14,8 @@ declare var google;
   selector: 'page-choosing-page',
   templateUrl: 'choosing-page.html'
 })
-export class ChoosingPage {copySources: { title: string; position: any; }[];copyDests: any[];
+export class ChoosingPage {
+    copySources: { title: string; position: any; }[]; copyDests: any[];
   sources: Array<{ title: string, position: any }>;
   dests: Array<{ title: string, position: any }>;
   sourceLoc: any;
@@ -27,12 +28,22 @@ export class ChoosingPage {copySources: { title: string; position: any; }[];copy
   floor: any;
   floorsData: any;
   gotFloors: any;
+  showDestList: any;
+  showSrcList: any;
+  globalSrc: any;
+  globalDest: any;
+  firstSrc: any;
+  firstDest: any;
   constructor(public navCtrl: NavController, public navParams: NavParams, public locService: LocationService,
-        public alertCtrl: AlertController) {
+    public alertCtrl: AlertController) {
     this.googleObj = navParams.get('googleObj');
     this.sources = [];
     this.dests = [];
     this.gotFloors = false;
+    this.showDestList = false;
+    this.showSrcList = false;
+    this.firstSrc = true
+    this.firstDest = true;
     locService.getLocations(this.sources, this.dests, this.googleObj);
     this.floorsData = [];
     locService.getFloors(this.floorsData, this);
@@ -40,7 +51,7 @@ export class ChoosingPage {copySources: { title: string; position: any; }[];copy
     google.maps.event.clearListeners(this.mapPage.mapView, 'mousemove');
 
   }
-  
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad ChoosingPage');
   }
@@ -49,26 +60,49 @@ export class ChoosingPage {copySources: { title: string; position: any; }[];copy
     this.mapPage.go();
     this.navCtrl.pop();
   }
-  
-//   getItemsDest ( ev: any) {
-//        let val = ev.target.value; 
-//    if (val && val.trim!='') {
-//        this.copyDests = this.dests.slice(); 
-//        this.copyDests = this.copyDests.filter((p) => {
-//        return (p.title.toLowerCase().indexOf(val.toLowerCase()) > -1);
-//      });
-//      this.showDestList=true; 
-//    } else {
-//      this.showDestList = false; 
-//  }
-//}
-  
+
+  getItemsDest(ev: any) {
+    let val = ev.target.value;
+     if (this.firstDest == true) {
+        this.firstDest = false;
+        this.globalDest = this.dests.slice(); 
+    }
+    if (val && val.trim != '') {
+      this.copyDests = this.globalDest.slice();
+      this.dests = this.copyDests.filter((p) => {
+        return (p.title.toLowerCase().startsWith(val.toLowerCase()));
+      });
+      this.showDestList = true;
+    } else {
+      this.showDestList = false;
+    }
+  }
+
+
+  getItemsSrc(ev: any) {
+    let val = ev.target.value;
+    if (this.firstSrc == true) {
+        this.firstSrc = false;
+        this.globalSrc = this.sources.slice(); 
+    }
+    if (val && val.trim != '') {
+      this.copySources = this.globalSrc.slice();
+      this.sources = this.copySources.filter((p) => {
+        return (p.title.toLowerCase().startsWith(val.toLowerCase()));
+      });
+      this.showSrcList = true;
+    } else {
+      this.showSrcList = false;
+    }
+  }
+
+
   showRadio(dict): Promise<boolean> {
     let pageObj = this;
     return new Promise((resolve, reject) => {
       let alert = this.alertCtrl.create();
       alert.setTitle('Choose Floor');
-      for(var i = 0; i < dict.length; i+=1) {
+      for (var i = 0; i < dict.length; i += 1) {
         alert.addInput({
           type: 'radio',
           label: dict[i]["id"],
@@ -88,34 +122,21 @@ export class ChoosingPage {copySources: { title: string; position: any; }[];copy
       alert.present();
     });
   }
-  
-  rememberDest(dict: any) {
-     let page = this;
-     if (this.floorsData[dict["title"]].length == 0) {
-        this.mapPage.setDstPosition(dict["position"], dict["title"]);
-        return;
-     }
-     this.showRadio(this.floorsData[dict["title"]]).then((result) => {
-        console.log(page.floor);
-        this.mapPage.setDstPosition(dict["position"], dict["title"]);
-        this.mapPage.setIndoorDescription(this.floor);
-     })
-  }
-  
-//  getItemsSrc ( ev: any) {
-//        let val = ev.target.value; 
-//    if (val && val.trim!='') {
-//      this.copySources = this.sources.slice();
-//      this.copySources=this.copySources.filter((p) => {
-//        return (p.title.toLowerCase().indexOf(val.toLowerCase()) > -1);
-//      });
-//      this.showSrcList = true; 
-//    } else {
-//      this.showSrcList = false; 
-//  }
-//}
 
-  rememberSrc(dict: any ) {
+  rememberDest(dict: any) {
+    let page = this;
+    if (this.floorsData[dict["title"]].length == 0) {
+      this.mapPage.setDstPosition(dict["position"], dict["title"]);
+      return;
+    }
+    this.showRadio(this.floorsData[dict["title"]]).then((result) => {
+      console.log(page.floor);
+      this.mapPage.setDstPosition(dict["position"], dict["title"]);
+      this.mapPage.setIndoorDescription(this.floor);
+    })
+  }
+
+  rememberSrc(dict: any) {
     this.mapPage.setSrcPosition(dict["position"], dict["title"]);
   }
 
@@ -123,6 +144,6 @@ export class ChoosingPage {copySources: { title: string; position: any; }[];copy
     this.mapPage.wantRecordRoute = value.checked;
   }
   changeToSimulation(value: any) {
-    this.mapPage.simulationMode=value.checked;
+    this.mapPage.simulationMode = value.checked;
   }
 }
