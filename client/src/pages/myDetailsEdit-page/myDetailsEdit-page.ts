0acc05269c405f587711d8747be4c6023095e004
Shahar-Y <shahar95@campus.technion.ac.il>
2017-06-24 18:10:38 +0300

    Changing to heroku server for Shay to upload the new service

--- client/src/pages/myDetailsEdit-page/myDetailsEdit-page.ts
+++ client/src/pages/myDetailsEdit-page/myDetailsEdit-page.ts
@@ -39,7 +39,7 @@ export class MyDetailsEditPage {
             this.navCtrl.push(LoginPage);
     }
     
-    presentAlert(str, myTitle) {
+    presentAlert(str, myTitle) {https://marketplace.eclipse.org/content/spartan-refactoring-0
         let alert = this.alertCtrl.create({
             title: myTitle,
             subTitle: str,
--- client/src/providers/myDetails-service.ts
+++ client/src/providers/myDetails-service.ts
@@ -32,7 +32,7 @@ export class MyDetailsService {
         var headers = new Headers();
         headers.append('Content-Type', 'application/x-www-form-urlencoded');
 
-        return this.http.post('http://localhost:8080/User/ChangeDetails', value, { headers: headers })
+        return this.http.post('https://spring-boot-nav.herokuapp.com/User/ChangeDetails', value, { headers: headers })
         .map(res => res.json()).catch(this.handleError);;
         
     }
