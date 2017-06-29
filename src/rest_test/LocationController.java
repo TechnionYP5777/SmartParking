f138f678602dcbd2fdae70d42746eecba3ff9269
DavidCohen55 <dcohen484@gmail.com>
2017-06-29 15:41:56 +0300

    Add comments to services #74

--- src/rest_test/LocationController.java
+++ src/rest_test/LocationController.java
@@ -29,12 +29,24 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class LocationController {
+	// A map from the destination name to its coordinates
 	Map<String, MapLocation> hmap;
+
+	// An arrayList of all of the Parking slots in the program
 	ArrayList<ServerParkingSlot> parkingList;
+
+	// TODO - change to map of Identifier,ServerParkingSlot
 	ServerParkingSlot sps;
+
+	// A ParkingAreas object
 	ParkingAreas areas;
+
+	// An object the include all of the areas at the database
 	Set<ServerParkingArea> parkingAreas;
 
+	/**
+	 * Initialize the fields above
+	 */
 	public LocationController() {
 		DBManager.initialize();
 		areas = new ParkingAreas();
@@ -42,6 +54,9 @@ public class LocationController {
 		setUpParkingSpots();
 	}
 
+	/**
+	 * Initialize the hmap to have all of the coordinates
+	 */
 	void setUpLocations() {
 		final ParseQuery<ParseObject> query = ParseQuery.getQuery("Destination");
 		hmap = new HashMap<String, MapLocation>();
@@ -58,6 +73,9 @@ public class LocationController {
 		}
 	}
 
+	/**
+	 * Initialize the parkingList to have all of the parking slots
+	 */
 	void setUpParkingSpots() {
 		final ParseQuery<ParseObject> query = ParseQuery.getQuery("ParkingSlot");
 		parkingList = new ArrayList<ServerParkingSlot>();
@@ -72,6 +90,10 @@ public class LocationController {
 		}
 	}
 
+	/**
+	 * @return a string of JSONObject for all of the Locations names and
+	 *         coordinates
+	 */
 	@CrossOrigin(origins = "http://localhost:8100")
 	@RequestMapping(value = "/Locations", produces = "application/json")
 	@ResponseBody
@@ -79,7 +101,11 @@ public class LocationController {
 		setUpLocations();
 		return new JSONObject(hmap) + "";
 	}
-	
+
+	// TODO - update the service to work with database
+	/**
+	 * @return for each destination its floors with the navigation description
+	 */
 	@CrossOrigin(origins = "http://localhost:8100")
 	@RequestMapping(value = "/Floors", produces = "application/json")
 	@ResponseBody
@@ -104,6 +130,13 @@ public class LocationController {
 		return new JSONObject(floors) + "";
 	}
 
+	/**
+	 * Getting information of a specific Destination according to name
+	 * 
+	 * @param name
+	 *            the location name
+	 * @return the coordinates of a destination
+	 */
 	@RequestMapping(value = "/Locations/{name}", produces = "application/json")
 	@ResponseBody
 	public MapLocation getLocation(@PathVariable String name) {
@@ -111,6 +144,9 @@ public class LocationController {
 		return hmap.get(name);
 	}
 
+	/**
+	 * @return all of the parking slots
+	 */
 	@RequestMapping(value = "/ParkingSlots", produces = "application/json")
 	@ResponseBody
 	public ArrayList<ServerParkingSlot> getParkingSlots() {
@@ -118,6 +154,13 @@ public class LocationController {
 		return parkingList;
 	}
 
+	/**
+	 * Getting information of a specific parking slot according to name
+	 * 
+	 * @param name
+	 *            the parking slot name
+	 * @return the slots details
+	 */
 	@RequestMapping(value = "/ParkingSlots/{name}", produces = "application/json")
 	@ResponseBody
 	public ServerParkingSlot getPark(@PathVariable String name) {
@@ -127,6 +170,9 @@ public class LocationController {
 		return null;
 	}
 
+	/**
+	 * @return all of the parking areas
+	 */
 	@CrossOrigin(origins = "http://localhost:8100")
 	@RequestMapping(value = "/ParkingAreas", produces = "application/json")
 	@ResponseBody
@@ -137,6 +183,13 @@ public class LocationController {
 		return parkingAreas;
 	}
 
+	/**
+	 * Getting information of a specific parking area according to name
+	 * 
+	 * @param name
+	 *            the parking area name
+	 * @return the area details
+	 */
 	@RequestMapping(value = "/ParkingAreas/{name}", produces = "application/json")
 	@ResponseBody
 	public ServerParkingArea getArea(@PathVariable String name) {
@@ -145,7 +198,23 @@ public class LocationController {
 				return pa;
 		return null;
 	}
-	
+
+	// TODO - change carNumber to identifier
+	/**
+	 * Finding the optimal parking slot for a user and save it for him according
+	 * to the followings
+	 * 
+	 * @param carNumber
+	 *            the user car number as identifier
+	 * @param src
+	 *            the starting position for the navigation, a Destination class
+	 *            according to name
+	 * @param dest
+	 *            the ending position for the navigation, a Destination class
+	 *            according to name
+	 * @return the coordinates of the closest parking slot that been saved for
+	 *         the user
+	 */
 	@CrossOrigin(origins = "http://localhost:8100")
 	@RequestMapping(value = "/FindPark", method = RequestMethod.POST, produces = "application/json")
 	@ResponseBody
@@ -157,16 +226,16 @@ public class LocationController {
 
 			if (u.getCurrentParking() != null)
 				throw new AlreadyExists("You have a parking slot");
-				
+
 			u.addToLastPaths(src, dest);
 			MapLocation srcLocation = null;
-			if (src.startsWith("$")) {
-				String[] point = src.replace("$", "").substring(1, src.length()-2).split(",");
-				srcLocation = new MapLocation(Double.parseDouble(point[0].trim()), Double.parseDouble(point[1].trim()));
-			} else
+			if (!src.startsWith("$"))
 				srcLocation = new Destination(src).getEntrance();
-			sps = new ServerParkingSlot(
-					Navigation.closestParkingSlot(u, srcLocation, areas, new Destination(dest)));
+			else {
+				String[] point = src.replace("$", "").substring(1, src.length() - 2).split(",");
+				srcLocation = new MapLocation(Double.parseDouble(point[0].trim()), Double.parseDouble(point[1].trim()));
+			}
+			sps = new ServerParkingSlot(Navigation.closestParkingSlot(u, srcLocation, areas, new Destination(dest)));
 			return sps.getLocation();
 		} catch (ParseException | LoginException | NotExists e) {
 			System.out.println("exception...");
@@ -175,24 +244,36 @@ public class LocationController {
 		}
 		return null;
 	}
-	
+
+	/**
+	 * @return null if there isn't slot saved, and the slot info if we've found
+	 */
 	@CrossOrigin(origins = "http://localhost:8100")
 	@RequestMapping(value = "/FindPark", produces = "application/json")
 	@ResponseBody
 	public ServerParkingSlot getBestPark() {
 		return sps != null ? sps : null;
 	}
-	
+
+	// TODO - change to Identifier
+	/**
+	 * A user need to leave his old parking slot for getting another and navigating
+	 * @param carNumber finding a user according to it and empty his parking slot
+	 * @return
+	 */
 	@CrossOrigin(origins = "http://localhost:8100")
 	@RequestMapping(value = "/LeavePark", method = RequestMethod.POST, produces = "application/json")
 	@ResponseBody
 	public String leavePark(@RequestParam("car") String carNumber) {
+		JSONObject obj = new JSONObject();
 		try {
 			User u = new User(carNumber);
 			u.getCurrentParking().changeStatus(ParkingSlotStatus.FREE);
 			u.setCurrentParking(null);
 			sps = null;
-		} catch (Exception e) {} 
+		} catch (Exception e) {
+			return obj.put("Error", e.getMessage()) + "";
+		}
 		return "{}";
 	}
 }
--- src/rest_test/PathController.java
+++ src/rest_test/PathController.java
@@ -25,6 +25,16 @@ public class PathController {
 		DBManager.initialize();
 	}
 
+	/**
+	 * Get a RoutePath between area to dest
+	 * 
+	 * @param area
+	 *            the area from which we want the walking route
+	 * @param dest
+	 *            the destination to which we want to be at the end of the
+	 *            walking route
+	 * @return Json serialization of the PathRoot or an error Json
+	 */
 	@CrossOrigin(origins = "http://localhost:8100")
 	@RequestMapping(value = "/GetPath", produces = "application/json")
 	@ResponseBody
@@ -55,10 +65,20 @@ public class PathController {
 		return obj + "";
 	}
 
+	/**
+	 * Get a RoutePath between area to dest that include only the coordinates
+	 * 
+	 * @param area
+	 *            the area from which we want the walking route
+	 * @param dest
+	 *            the destination to which we want to be at the end of the
+	 *            walking route
+	 * @return Json serialization of the PathRoot coordinates or an error Json
+	 */
 	@CrossOrigin(origins = "http://localhost:8100")
 	@RequestMapping(value = "/GetRoute", produces = "application/json")
 	@ResponseBody
-	public String getRoot(@RequestParam("area") String area, @RequestParam("dest") String dest) {
+	public String GetRoute(@RequestParam("area") String area, @RequestParam("dest") String dest) {
 
 		JSONObject obj = new JSONObject();
 		try {
@@ -81,12 +101,14 @@ public class PathController {
 		return obj + "";
 	}
 
-	@RequestMapping(value = "/checkpost", method = RequestMethod.POST, produces = "application/json")
-	@ResponseBody
-	public WrapObj post(@RequestBody WrapObj o) {
-		return o;
-	}
-
+	/**
+	 * Send a RoutePath as a WrapObj (a class that used to deserialize Json to
+	 * similar class) to the database and trying to add it.
+	 * 
+	 * @param o
+	 *            the routePath as WrapObj will be changed to RoutePath
+	 * @return true if added correctly else false
+	 */
 	@RequestMapping(value = "/SendPath", method = RequestMethod.POST, produces = "application/json")
 	@ResponseBody
 	public boolean sendPath(@RequestBody WrapObj o) {
@@ -99,6 +121,14 @@ public class PathController {
 		}
 	}
 
+	/**
+	 * Getting the user last search routes
+	 * @param user
+	 *            the identification of the user from whom we want to get the
+	 *            last search paths
+	 * @return String of JSONObject which include the sources & destinations
+	 *         that the user searched last
+	 */
 	@RequestMapping(value = "/GetLastPaths", method = RequestMethod.GET, produces = "application/json")
 	@ResponseBody
 	public String getLastPaths(@RequestParam("userName") String user) {
@@ -110,13 +140,13 @@ public class PathController {
 				obj.put("Status", "There isn't any saved paths");
 			else {
 				JSONArray list = new JSONArray();
-				for (String route : lastRoutes){
+				for (String route : lastRoutes) {
 					JSONObject r = new JSONObject();
 					r.put("src", route.split("\\$")[0]);
 					r.put("dst", route.split("\\$")[1]);
 					list.put(r);
 				}
-				
+
 				obj.put("SavedPaths", list);
 			}
 		} catch (LoginException e) {
